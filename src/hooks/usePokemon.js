d628a289ce35a385bff4a55bd15df6678a990928
Rene Cardenas <renecar616@gmail.com>
2026-04-15 18:27:55 -0500

    Adding Cache-Control

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,7 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 
 async function fetchPokemon(nameId) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameId}`);
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameId}`, {
+    headers: {
+      "Cache-Control": "max-age=3600", // * Tells the browser to keep it for an hour.
+    },
+  });
   if (!res.ok) throw new Error("Pokemon not found");
   return res.json();
 }
